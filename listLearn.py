e2c29606a25c917bf3a1f544bc64caeb77ff13c0
ikmalrashidin <ikmal112004@gmail.com>
2025-10-17 09:09:48 +0800

    list commit

---
+++ listLearn.py
@@ -0,0 +1,17 @@
+countries = ['USA', 'Canada', 'Germany', 'UK', 'France']
+
+
+
+print(f"Here is the sorted list of countries:")
+print(sorted(countries))
+
+print(f"Original countries list:")
+print(countries)
+
+print(f"Here is the reverse sorted list of countries:")
+print(sorted(countries, reverse=True))
+
+print(f"Reverse list:")
+(countries.reverse())
+print(countries)
+

28a3ffc8db3cd7267f1921ec558cc7cd6e2edf16
ikmalrashidin <ikmalhishampro@gmail.com>
2025-10-19 15:11:03 +0800

    dictionaries commit

---
+++ dictionaries.py
@@ -0,0 +1,20 @@
+alien = {'x_position': 0, 'y_position': 25, 'speed': 'slow'}
+
+print(f"Original position: {alien['x_position']}, {alien['y_position']}")
+
+if alien['speed'] == 'slow':
+    x_increment = 1
+elif alien['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien['x_position'] = alien['x_position'] + x_increment
+
+print(alien['x_position'])
+
+
+
+
+
+
